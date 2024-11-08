e8dbc5133e13756967d37e238e525185cf91ca44
NearGoez <Slythin07@gmail.com>
2024-11-08 13:51:10 -0300

    segundo commit -> implementar cookis

--- scraper.py
+++ scraper.py
@@ -2,4 +2,24 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
+s = requests.Session()
+
+
+
+tutor_init = s.get('https://ipsdatax.com/tutor-login/')
+
+
+login_payload = {'log': "DbenjaminDofus@gmail.com", 
+                 'pwd': "214ff34fa"
+
+        }
+
+tutor_login = s.post(url = 'https://ipsdatax.com/tutor-login/', data = login_payload)
+
+login_cookies = {
+        'PHPSESSID'
+
+        }
+
+print(tutor_login.cookies)
 
